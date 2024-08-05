1fb381d1104e59b53d2926894884460c6e045411
falmogadem <falmogadem@isb.sa>
2024-08-06 00:00:19 +0300

    init

---
+++ Src/ShopApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductRequest(Guid Id, string Name, string Description,
+    List<string> Category, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid id);
+
+public class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResult>();
+            return Results.Created($"/products/{response.id}", response);
+        })
+            .WithName("CreateProduct")
+            .WithDescription("Create Product")
+            .WithSummary("Create Product")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ Src/ShopApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+using Catalog.API.Models;
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand( Guid Id,string Name,string Description ,List<string> Category ,string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>
+    {
+
+    }
+    public record CreateProductResult(Guid id);
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+           var produnct =  new Product
+           {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
